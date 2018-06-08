60dba6da706d52992de449a35deeb54641c020e6
lq <lqing1988@126.com>
2018-06-08 11:18:07 +0800

    new hello add 3,4

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world111111");
 		System.out.println("hello world222222");
+		System.out.println("hello world333333333333");
 	}
 	
 }
