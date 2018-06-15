4aaaaf289d8ae618765608acf3c58b72edcce8a0
lq <lqing1988@126.com>
2018-06-15 10:30:47 +0800

    modeified hello.java

--- Hello.java
+++ Hello.java
@@ -3,10 +3,7 @@ package com.zhiyou.emp.entity;
 public class Hello {
 	
 	public static void main(String[] args) {
-		System.out.println("hello world111111");
-		System.out.println("hello world222222");
-		System.out.println("hello world333333333333");
-		System.out.println("hello world6666666666666");
+		System.out.print("修改brh分支的内容为显示1行");
 	}
 	
 }
