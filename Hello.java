b8413afb3559fd7c1c6370f113cb209ea9286304
lq <lqing1988@126.com>
2018-06-15 10:52:14 +0800

    url print

--- Hello.java
+++ Hello.java
@@ -3,8 +3,10 @@ package com.zhiyou.emp.entity;
 public class Hello {
 	
 	public static void main(String[] args) {
-		System.out.print("修改brh分支的内容为显示1行");
-		System.out.print("修改brh分支的内容为显示2行");
+		System.out.print("修改master分支的内容为显示1行");
+		System.out.print("修master分支的内容为显示2行");
+		System.out.print("修master分支的内容为显示3行");
+		System.out.print("修master分支的内容为显示4行");
 	}
 	
 }
